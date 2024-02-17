7cfc3aa13ba6d76d4db920e264898ff4140bc30d
mostsfa538 <mostafa51moktar@gmail.com>
2024-02-17 14:38:46 +0200

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 /**
-* selection_sort -  sorts an array of integers in ascending order 
+* selection_sort -  sorts an array of integers in ascending order
 * using the Selection sort algorithm
 * @array: the array
 * @size: the size of array
@@ -14,28 +14,28 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i = 0, j = 1, min = 0, temp, limit = size - 1;
+size_t i = 0, j = 1, min = 0, temp, limit = size - 1;
 
-    if (size < 2)
-        return;
+if (size < 2)
+return;
 
-    while (i < limit)
+while (i < limit)
+{
+	if (j == size)
 	{
-		if (j == size)
+		if (i != min)
 		{
-			if (i != min)
-			{
-				temp = array[i];
-				array[i] = array[min];
-				array[min] = temp;
-				print_array(array, size);
-			}
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+}
 
-			++i;
-			min = i;
-			j = i + 1;
-			continue;
-		}
+		++i;
+		min = i;
+		j = i + 1;
+		continue;
+	}
 
 		if (array[min] > array[j])
 			min = j;
