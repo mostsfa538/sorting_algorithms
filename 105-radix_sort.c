0377321e44740c4d51fe4892481f6e69a8d31c19
mostsfa538 <mostafa51moktar@gmail.com>
2024-02-20 08:02:59 +0200

    radix sort

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -11,14 +11,43 @@
  *
  * Rrturn: void
  */
-void radix_sort(int *array, size_t size) {
+void radix_sort(int *array, size_t size)
+{
+    size_t i;
     int max = find_max(array, size), exp;
+    int *output, *count;
+
+
+     output = (int *)malloc(size * sizeof(int));
+    if (!output)
+    {
+        return;
+    }
+
+    count = (int *)malloc(sizeof(int) * 10);
+    if (!count)
+    {
+        free(output);
+        return;
+    }
 
     for (exp = 1; max / exp > 0; exp *= 10)
     {
-        count_sort(array, size, exp);
+        for (i = 0; i < size; i++)
+        count[(array[i] / exp) % 10]++;
+
+        for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+        for (i = size; i > 0; i--)
+            output[--count[(array[i - 1] / exp) % 10]] = array[i - 1];
+
+        for (i = 0; i < size; i++)
+            array[i] = output[i];
         print_array(array, size);
     }
+    free(count);
+    free(output);
 }
 
 /**
@@ -37,31 +66,4 @@ int find_max(int *array, size_t size)
             max = array[i];
 
     return (max);
-}
-
-void count_sort(int *array, size_t size, int exp)
-{
-    size_t i;
-    int *output, *count;
-
-    output = (int *)malloc(size * sizeof(int));
-    if (!output)
-    {
-        exit(EXIT_FAILURE);
-    }
-
-    count = (int *)malloc(sizeof(int) * 10);
-
-    for (i = 0; i < size; i++)
-        count[(array[i] / exp) % 10]++;
-
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
-
-    for (i = size; i > 0; i--) {
-        output[--count[(array[i - 1] / exp) % 10]] = array[i - 1];
-    }
-
-    for (i = 0; i < size; i++)
-        array[i] = output[i];
 }
